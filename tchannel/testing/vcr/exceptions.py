f83ddb15165d4ec9ef28183447d7ee2726d68da2
Abhinav Gupta <abg@uber.com>
2015-07-23 12:03:24 -0700

    Rename VCRException to VCRError

--- tchannel/testing/vcr/exceptions.py
+++ tchannel/testing/vcr/exceptions.py
@@ -1,13 +1,13 @@
 from __future__ import absolute_import
 
 
-class VCRException(Exception):
+class VCRError(Exception):
     "Base class for exceptions raised by the VCR library."
 
 
-class RequestNotFoundError(VCRException):
+class RequestNotFoundError(VCRError):
     "Raised when a request doesn't have a recorded response."
 
 
-class UnsupportedVersionError(VCRException):
+class UnsupportedVersionError(VCRError):
     "Raised when the version of a recording is not supported."
