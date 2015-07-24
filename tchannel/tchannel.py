06d27df35c11a2712d8e8c0e7c8cd577004d0a08
Grayson Koonce <grayson@uber.com>
2015-07-24 16:54:03 -0700

    sketching methods

--- tchannel/tchannel.py
+++ tchannel/tchannel.py
@@ -2,10 +2,25 @@ from __future__ import (
     absolute_import, division, print_function, unicode_literals
 )
 
-from .tornado.tchannel import TChannel as NodeInspiredTChannel
+from .tornado import TChannel as NodeInspiredTChannel
+
+__all__ = ['TChannel']
 
 
 class TChannel(NodeInspiredTChannel):
 
-    def call(self):
+    def call(self, argscheme):
         pass
+
+    def call_raw(self):
+        pass
+
+    def call_json(self):
+        pass
+
+    def call_http(self):
+        pass
+
+    def call_thrift(self):
+        pass
+
