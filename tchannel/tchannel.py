4bdfe279c72abb47d9fe1e28bdea11e16c250809
Grayson Koonce <grayson@uber.com>
2015-07-24 17:03:06 -0700

    things are coroutines

--- tchannel/tchannel.py
+++ tchannel/tchannel.py
@@ -2,6 +2,8 @@ from __future__ import (
     absolute_import, division, print_function, unicode_literals
 )
 
+from tornado import gen
+
 from .glossary import DEFAULT_TIMEOUT
 from .tornado import TChannel as NodeInspiredTChannel
 
@@ -10,18 +12,23 @@ __all__ = ['TChannel']
 
 class TChannel(NodeInspiredTChannel):
 
+    @gen.coroutine
     def call(self, argscheme, service, endpoint, body, headers={}, timeout=DEFAULT_TIMEOUT):
         pass
 
+    @gen.coroutine
     def call_raw(self):
         pass
 
+    @gen.coroutine
     def call_json(self):
         pass
 
+    @gen.coroutine
     def call_http(self):
         pass
 
+    @gen.coroutine
     def call_thrift(self):
         pass
 
