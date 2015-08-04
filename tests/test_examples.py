3e79307391db0702989fe7b4092f4d497c9f1739
Bryce Lampe <blampe@uber.com>
2015-08-03 17:34:55 -0700

    fix flakey example tests

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -23,23 +23,21 @@ import os
 import subprocess
 
 import pytest
-
-try:
-    import __pypy__  # noqa
-except ImportError:
-    PYPY = False
-else:
-    PYPY = True
+import psutil
 
 
 @contextlib.contextmanager
-def popen(path):
-    process = subprocess.Popen(
+def popen(path, wait_for_listen=False):
+    process = psutil.Popen(
         ['python', path],
         stdout=subprocess.PIPE,
         stderr=subprocess.PIPE,
     )
-    process.poll()
+
+    if wait_for_listen:
+        conns = None
+        while process.is_running() and not conns:
+            conns = [x for x in process.connections() if x.status == psutil.CONN_LISTEN]
 
     try:
         yield process
@@ -62,7 +60,6 @@ def examples_dir():
         os.chdir(cwd)
 
 
-@pytest.mark.skipif(PYPY, reason='flaky in pypy')
 @pytest.mark.parametrize(
     'example_type',
     [
@@ -86,10 +83,7 @@ def test_example(examples_dir, example_type):
         example_type + 'client.py',
     )
 
-    with popen(server_path):
-        # :(
-        import time
-        time.sleep(0.05)
+    with popen(server_path, wait_for_listen=True):
         with popen(client_path) as client:
             assert (
                 client.stdout.read() == 'Hello, world!\n'
