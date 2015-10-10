02a65d61dfb0f0ede61489aa1d959a001f03e473
Bryce Lampe <blampe@uber.com>
2015-10-09 18:21:49 -0700

    tchannel is compatible with thriftrw 0.4

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
         'crcmod',
         'tornado>=4.0,<5.0',
         'toro>=0.8,<0.9',
-        'thriftrw>=0.3,<0.4',
+        'thriftrw>=0.3,<0.5',
         'threadloop>=0.5,<0.6',
         'futures',
     ],
