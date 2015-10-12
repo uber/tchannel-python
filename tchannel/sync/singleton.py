d524948706530fef3bfe021c7ab81e4babcaae0c
Grayson Koonce <grayson@uber.com>
2015-10-12 12:00:18 -0700

    sync singleton gets it's own thread local storage

--- tchannel/sync/singleton.py
+++ tchannel/sync/singleton.py
@@ -2,11 +2,16 @@ from __future__ import (
     absolute_import, division, print_function, unicode_literals
 )
 
+from threading import local
+
 from tchannel.singleton import TChannel as TChannelSingleton
 
 from .client import TChannel as SyncTChannel
 
 
 class TChannel(TChannelSingleton):
+
+    local = local()
+    local.tchannel = None
 
     tchannel_cls = SyncTChannel
