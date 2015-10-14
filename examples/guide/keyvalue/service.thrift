0a0fa430401cde475d79a937359e67715544f6cf
Bryce Lampe <blampe@uber.com>
2015-10-13 18:42:11 -0700

    update guide to use thrift.load

--- examples/guide/keyvalue/service.thrift
+++ examples/guide/keyvalue/service.thrift
@@ -19,19 +19,19 @@
 # THE SOFTWARE.
 
 exception NotFoundError {
-    1: optional string key,
+    1: required string key,
 }
 
 service KeyValue {
 
     string getValue(
-        1: string key,
+        1: required string key,
     ) throws (
         1: NotFoundError notFound,
     )
 
     void setValue(
-        1: string key,
-        2: string value,
+        1: required string key,
+        2: required string value,
     )
 }
