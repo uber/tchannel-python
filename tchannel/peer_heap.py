ec78055e92e1cedbe53b0d846d95812cb95cd06b
Junchao Wu <junchaow@uber.com>
2015-11-18 19:42:11 -0800

    Add peer heap class

---
+++ tchannel/peer_heap.py
@@ -0,0 +1,61 @@
+from __future__ import absolute_import
+
+from .container import heap
+from .container.heap import HeapOperation
+
+
+class PeerHeap(HeapOperation):
+    def __init__(self):
+        self.peers = []
+
+    def cmp(self, i, j):
+        return self.peers[i] < self.peers[j]
+
+    def push(self, x):
+        x.index = len(self.peers)
+        self.peers.append(x)
+
+    def pop(self):
+        n = len(self.peers)
+        item = self.peers[n-1]
+        item.index = -1
+        del self.peers[n - 1]
+        return item
+
+    def __len__(self):
+        return len(self.peers)
+
+    def swap(self, i, j):
+        self.peers[i], self.peers[j] = self.peers[j], self.peers[i]
+        self.peers[i].index = i
+        self.peers[j].index = j
+
+    def update_peer(self, peer):
+        """Update the peer's position in the heap after peer's score changed"""
+        heap.fix(self, peer.index)
+
+    def pop_peer(self):
+        """Pop the top peer from the heap
+
+        :return
+            return the top peer and remove it from the heap if heap is not
+            empty.Otherwise return None.
+        """
+        if len(self.peers) == 0:
+            return None
+        return heap.pop(self)
+
+    def push_peer(self, peer):
+        """Push a new peer into the heap"""
+        heap.push(self, peer)
+
+    def peek_peer(self):
+        """Return the top peer of the heap
+        :return
+            return the top peer if heap is not empty. Otherwise return None.
+        """
+
+        if len(self.peers) == 0:
+            return None
+
+        return self.peers[0]
