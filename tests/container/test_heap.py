98e96be80a2685c6f812b05df8f3c33fdef3389d
Junchao Wu <junchaow@uber.com>
2015-11-20 15:14:55 -0800

    update peer test

--- tests/container/test_heap.py
+++ tests/container/test_heap.py
@@ -1,10 +1,11 @@
 from __future__ import absolute_import
 import random
 import sys
+import math
 import pytest
 import six
-from tchannel.container import heap
 
+from tchannel.container import heap
 from tchannel.container.heap import HeapOperation
 
 
@@ -22,16 +23,10 @@ class IntHeap(HeapOperation):
         self.values.append(x)
 
     def pop(self):
-        n = len(self.values)
-        item = self.values[n - 1]
-        del self.values[n - 1]
-        return item
+        return self.values.pop()
 
     def swap(self, i, j):
         self.values[i], self.values[j] = self.values[j], self.values[i]
-
-
-n = 1000
 
 
 @pytest.fixture
@@ -39,61 +34,71 @@ def int_heap():
     return IntHeap()
 
 
-def create_values(n):
-    values = []
-    for i in six.moves.range(n):
-        values.append(i)
+@pytest.fixture(params=['unique', 'duplicate', 'reverse'])
+def values(request):
+    n = 1000
+    v = []
 
-    random.shuffle(values)
-    return values
+    if request.param == "unique":
+        for i in six.moves.range(n):
+            v.append(i)
+        random.shuffle(v)
+    elif request.param == "duplicate":
+        for _ in six.moves.range(n):
+            v.append(random.randint(0, math.floor(n/20)))
+    elif request.param == "reverse":
+        for i in six.moves.range(n, -1, -1):
+            v.append(i)
+
+    return v
 
 
 def verify(ph, parent):
     child1 = 2 * parent + 1
     child2 = 2 * parent + 2
     if child2 < ph.size():
-        assert ph.lt(parent, child1)
+        assert not ph.lt(child1, parent)
         verify(ph, child1)
 
     if child2 < ph.size():
-        assert ph.lt(parent, child2)
+        assert not ph.lt(child2, parent)
         verify(ph, child2)
 
 
-def test_init(int_heap):
-    int_heap.values = create_values(n)
+def test_init(int_heap, values):
+    int_heap.values = values
     heap.init(int_heap)
     verify(int_heap, 0)
 
 
-def test_push(int_heap):
-    values = create_values(n)
+def test_push(int_heap, values):
     for value in values:
         heap.push(int_heap, value)
         verify(int_heap, 0)
 
-    assert int_heap.size() == n
+    assert int_heap.size() == len(values)
 
 
-def test_pop(int_heap):
-    values = create_values(n)
+def test_pop(int_heap, values):
     for value in values:
         heap.push(int_heap, value)
         verify(int_heap, 0)
 
-    for i in six.moves.range(n):
-        assert i == heap.pop(int_heap)
+    n = len(values)
+    for _ in six.moves.range(n):
+        heap.pop(int_heap)
+        verify(int_heap, 0)
 
     assert int_heap.size() == 0
 
 
-def test_remove(int_heap):
-    values = create_values(n)
+def test_remove(int_heap, values):
     for value in values:
         heap.push(int_heap, value)
         verify(int_heap, 0)
 
     # random remove item from the heap
+    n = len(values)
     for i in six.moves.range(n - 1, -1, -1):
         heap.remove(int_heap, random.randint(0, i))
         verify(int_heap, 0)
@@ -102,7 +107,7 @@ def test_remove(int_heap):
 @pytest.mark.heapfuzz
 @pytest.mark.skipif(True, reason='stress test for the value heap operations')
 def test_heap_fuzz(int_heap):
-    for i in six.moves.range(random.randint(1, 100000)):
+    for _ in six.moves.range(random.randint(1, 100000)):
         ops = random.randint(0, 1)
         if ops == 0:  # push
             heap.push(int_heap, random.randint(0, sys.maxint))
@@ -116,9 +121,9 @@ def test_heap_fuzz(int_heap):
         verify(int_heap, 0)
 
 
-def smallest(values):
+def smallest(vs):
     m = sys.maxint
-    for value in values:
+    for value in vs:
         if value < m:
             m = value
     return m
--- tests/test_peer_heap.py
+++ tests/test_peer_heap.py
@@ -1,18 +1,36 @@
 from __future__ import absolute_import
 import sys
+import math
 
 import mock
 import random
 import pytest
+import six
 from tchannel.peer_heap import PeerHeap
-
-
-n = 1000
 
 
 @pytest.fixture
 def peer_heap():
     return PeerHeap()
+
+
+@pytest.fixture(params=['unique', 'duplicate', 'reverse'])
+def peers(request):
+    n = 1000
+    v = []
+
+    if request.param == "unique":
+        for i in six.moves.range(n):
+            v.append(mock_peer(i))
+        random.shuffle(v)
+    elif request.param == "duplicate":
+        for _ in six.moves.range(n):
+            v.append(mock_peer(random.randint(0, math.floor(n/20))))
+    elif request.param == "reverse":
+        for i in six.moves.range(n, -1, -1):
+            v.append(mock_peer(i))
+
+    return v
 
 
 def mock_peer(score=None):
@@ -22,59 +40,49 @@ def mock_peer(score=None):
     return peer
 
 
-def mock_peers(n):
-    peers = []
-    for i in range(n):
-        peers.append(mock_peer(i))
-
-    random.shuffle(peers)
-    return peers
-
-
 def verify(ph, parent):
+    """Assert the peer is still well-maintained."""
     child1 = 2*parent + 1
     child2 = 2*parent + 2
     if child2 < ph.size():
-        assert ph.lt(parent, child1)
+        assert not ph.lt(child1, parent)
         verify(ph, child1)
 
     if child2 < ph.size():
-        assert ph.lt(parent, child2)
+        assert not ph.lt(child2, parent)
         verify(ph, child2)
 
 
-def test_push(peer_heap):
-    peers = mock_peers(n)
+def test_push(peer_heap, peers):
     for peer in peers:
         peer_heap.push_peer(peer)
         verify(peer_heap, 0)
 
-    assert peer_heap.size() == n
+    assert peer_heap.size() == len(peers)
 
 
-def test_pop(peer_heap):
-    peers = mock_peers(n)
+def test_pop(peer_heap, peers):
     for peer in peers:
         peer_heap.push_peer(peer)
         verify(peer_heap, 0)
 
-    for i in range(n):
-        assert i == peer_heap.pop_peer().score
+    n = len(peers)
+    for _ in six.moves.range(n):
+        peer_heap.pop_peer()
+        verify(peer_heap, 0)
 
     assert peer_heap.size() == 0
 
 
-def test_update(peer_heap):
-    peers = mock_peers(n)
+def test_update(peer_heap, peers):
     for peer in peers:
         peer_heap.push_peer(peer)
         verify(peer_heap, 0)
 
-    p = peer_heap.peers[n-1]
+    p = peer_heap.peers[len(peers) - 1]
     p.score = -1
     peer_heap.update_peer(p)
     verify(peer_heap, 0)
-
     assert peer_heap.peek_peer().score == -1
     assert peer_heap.peek_peer() == p
 
@@ -82,7 +90,7 @@ def test_update(peer_heap):
 @pytest.mark.heapfuzz
 @pytest.mark.skipif(True, reason='stress test for the peer heap operations')
 def test_heap_fuzz(peer_heap):
-    for i in range(random.randint(1, 1000000)):
+    for _ in six.moves.range(random.randint(1, 100000)):
         ops = random.randint(0, 2)
         if ops == 0:    # push
             peer_heap.push_peer(mock_peer())
@@ -101,9 +109,9 @@ def test_heap_fuzz(peer_heap):
         verify(peer_heap, 0)
 
 
-def smallest(peers):
+def smallest(ps):
     m = sys.maxint
-    for peer in peers:
+    for peer in ps:
         if peer.score < m:
             m = peer.score
 
