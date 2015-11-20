9a248406ef65713cd64a9e95cd4d890dea30a3e2
Junchao Wu <junchaow@uber.com>
2015-11-20 15:48:11 -0800

    update peer heap test

--- tests/container/test_heap.py
+++ tests/container/test_heap.py
@@ -1,3 +1,23 @@
+# Copyright (c) 2015 Uber Technologies, Inc.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
 from __future__ import absolute_import
 import random
 import sys
@@ -84,9 +104,8 @@ def test_pop(int_heap, values):
         heap.push(int_heap, value)
         verify(int_heap, 0)
 
-    n = len(values)
-    for _ in six.moves.range(n):
-        heap.pop(int_heap)
+    for i in sorted(values):
+        assert i == heap.pop(int_heap)
         verify(int_heap, 0)
 
     assert int_heap.size() == 0
