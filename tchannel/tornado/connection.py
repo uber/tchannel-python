56644af08688a09b33b12ae5321a0dd317369311
Junchao Wu <junchaow@uber.com>
2015-12-03 15:22:55 -0800

    Add docstring for total_outbound_pending

--- tchannel/tornado/connection.py
+++ tchannel/tornado/connection.py
@@ -143,6 +143,7 @@ class TornadoConnection(object):
 
     @property
     def total_outbound_pendings(self):
+        """Return total amount of outbounding pending req/res."""
         return (len(self._outbound_pending_res) +
                 len(self._outbound_pending_req))
 
