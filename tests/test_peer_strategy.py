3275141d39e06f81641c820df0bd0d74d9d0255e
Junchao Wu <junchaow@uber.com>
2015-12-11 17:41:55 -0800

    Add tests for tchannel peer strategies

---
+++ tests/test_peer_strategy.py
@@ -0,0 +1,62 @@
+# Copyright (c) 2015 Uber Technologies, Inc.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+from __future__ import absolute_import
+
+import sys
+
+import pytest
+from tchannel import TChannel
+from tchannel.peer_strategy import PreferIncomingCalculator
+from tchannel.tornado.connection import TornadoConnection
+from tchannel.tornado.connection import INCOMING
+from tchannel.tornado.peer import Peer
+
+
+@pytest.mark.gen_test
+def test_get_score():
+    server = TChannel('server')
+    server.listen()
+    connection = yield TornadoConnection.outgoing(server.hostport)
+
+    peer = Peer(TChannel('test'), '10.10.101.21:230')
+    calculator = PreferIncomingCalculator()
+    assert sys.maxint == calculator.get_score(peer)
+
+    peer.register_outgoing_conn(connection)
+    assert sys.maxint == calculator.get_score(peer)
+
+    connection.direction = INCOMING
+    assert sys.maxint != calculator.get_score(peer)
+
+    peer.port = 0
+    peer.host = '1.1.1.1'
+    calculator = PreferIncomingCalculator()
+    assert sys.maxint == calculator.get_score(peer)
+
+    peer.host = '0.0.0.0'
+    peer.port = 98
+    calculator = PreferIncomingCalculator()
+    assert sys.maxint == calculator.get_score(peer)
+
+    peer.host = '0.0.0.0'
+    peer.port = 0
+    calculator = PreferIncomingCalculator()
+    assert sys.maxint == calculator.get_score(peer)
