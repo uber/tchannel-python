e0ffcfe15135a7ee3a570356db203f3fabc27234
Abhinav Gupta <abg@uber.com>
2016-10-05 14:02:30 -0700

    Back to development: 0.28.4

--- tchannel/__init__.py
+++ tchannel/__init__.py
@@ -22,7 +22,7 @@ from __future__ import (
     absolute_import, division, print_function, unicode_literals
 )
 
-__version__ = '0.28.3'
+__version__ = '0.28.4.dev0'
 # Update setup.py when changing this. zest.releaser doesn't support updating
 # both of them yet.
 
