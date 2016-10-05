018508fe9deea4bea2504169e8003ab3423115b8
Abhinav Gupta <abg@uber.com>
2016-10-05 14:05:39 -0700

    Preparing release 0.29.1

--- tchannel/__init__.py
+++ tchannel/__init__.py
@@ -22,7 +22,7 @@ from __future__ import (
     absolute_import, division, print_function, unicode_literals
 )
 
-__version__ = '0.29.0'
+__version__ = '0.29.1'
 # Update setup.py when changing this. zest.releaser doesn't support updating
 # both of them yet.
 
