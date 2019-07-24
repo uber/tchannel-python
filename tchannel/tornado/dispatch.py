f5d40f410326b8961fa650317e012eb5aa20c1b8
James Westover <westover@uber.com>
2019-07-23 18:34:45 -0700

    Clean up some dirty logic

--- tchannel/tornado/dispatch.py
+++ tchannel/tornado/dispatch.py
@@ -129,9 +129,7 @@ class RequestDispatcher(object):
         chunk = yield request.argstreams[0].read()
         while chunk:
             if six.PY3:
-                if isinstance(chunk, str) and \
-                        isinstance(request.endpoint, bytes) or \
-                        isinstance(request.endpoint, str) and \
+                if isinstance(request.endpoint, str) and \
                         isinstance(chunk, bytes):
                     chunk = chunk.decode('utf8')
 
